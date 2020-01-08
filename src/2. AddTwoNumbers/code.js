84059a004ecaed9a418922e3e2fdade67c606e74
SeongMin Han <hsm950825@naver.com>
2020-01-08 15:13:52 +0900

    Add Two Numbers (20.01.08)

---
+++ src/2. AddTwoNumbers/code.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+let sum = 0;
+let current = new ListNode(0);
+let result = current;
+
+while (l1 || l2) {
+  if (l1) {
+    sum += l1.val;
+    l1 = l1.next;
+  }
+
+  if (l2) {
+    sum += l2.val;
+    l2 = l2.next;
+  }
+
+  current.next = new ListNode(sum % 10);
+  current = current.next;
+
+  sum = sum > 9 ? 1 : 0;
+}
+
+if (sum) {
+  current.next = new ListNode(sum);
+}
+
+return result.next;
