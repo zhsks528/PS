afd66976353e4f7820181ce8cee84c77a5825789
SeongMin Han <hsm950825@naver.com>
2021-01-26 22:28:44 +0900

    [Add] SWEA 1244 [S/W 문제해결 응용] 2일차 - 최대 상금

---
+++ SWEA/Level3/1244.py
@@ -0,0 +1,40 @@
+# 35분
+# 도움&참조 : O
+
+
+def dfs(count):
+    global answer
+
+    if not count:
+        temp = int(''.join(values))
+
+        if answer < temp:
+            answer = temp
+        return
+
+    for i in range(length):
+
+        for j in range(i+1, length):
+            values[i], values[j] = values[j], values[i]
+            temp_key = ''.join(values)
+
+            if visited.get((temp_key, count-1), 1):
+
+                visited[(temp_key, count-1)] = 0
+                dfs(count-1)
+            values[i], values[j] = values[j], values[i]
+
+
+for t in range(int(input())):
+    answer = -1
+    value, change = input().split()
+
+    values = list(value)
+    change = int(change)
+
+    length = len(values)
+
+    visited = {}
+    dfs(change)
+
+    print('#{} {}'.format(t+1, answer))
